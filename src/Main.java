11d11f6b9f60c2e793d91158702b5e03bace2762
keons <fsomething180@proton.me>
2025-12-18 17:49:57 +0500

    main class with object comparison and output

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    static void main() {
+        Supplier mainSupplier = new Supplier("Global Tech", "contact@globaltech.com");
+        Inventory warehouse = new Inventory("Main Warehouse", 500);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        Product p1 = new Product("Laptop", 400000.00, 15);
+        Product p2 = new Product("Mouse", 15000.00, 15);
+
+        System.out.println("--- System Setup ---");
+        System.out.println(mainSupplier);
+        System.out.println(warehouse);
+        System.out.println("\n--- Current Products ---");
+        System.out.println(p1);
+        System.out.println(p2);
+
+        System.out.println("\n--- Stock Comparison ---");
+        if (p1.getQuantity() == p2.getQuantity()) {
+            System.out.println("Both " + p1.getName() + " and " + p2.getName() + " have the same stock level: " + p1.getQuantity());
+        } else {
+            System.out.println("Stock levels are different.");
+        }
     }
-}// mal
+}
